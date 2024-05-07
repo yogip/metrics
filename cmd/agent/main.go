fa4c3e371cea897b854bead718c4c8b648a7eb54
Пётр Павлов <pepavlov@sbercloud.ru>
2024-05-07 17:23:02 +0500

    Iter4 фикс флагов

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -10,19 +10,24 @@ import (
 )
 
 var flagRunAddr string
-var flagReportInterval time.Duration
-var flagPollInterval time.Duration
+
+var flagReportInterval int64
+var flagPollInterval int64
 
 func init() {
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "server addres and port to send metrics")
-	flag.DurationVar(&flagReportInterval, "r", 10*time.Second, "sent metric to server every given interval")
-	flag.DurationVar(&flagPollInterval, "p", 2*time.Second, "gather metric every given interval")
+	flag.Int64Var(&flagReportInterval, "r", 10, "sent metric to server every given interval")
+	flag.Int64Var(&flagPollInterval, "p", 2, "gather metric every given interval")
 }
 
 func main() {
 	flag.Parse()
 
-	cfg := config.NewAgentConfig(flagRunAddr, flagReportInterval, flagPollInterval)
+	cfg := config.NewAgentConfig(
+		flagRunAddr,
+		time.Duration(flagReportInterval)*time.Second,
+		time.Duration(flagPollInterval)*time.Second,
+	)
 
 	log.Println("Start agent for: ", cfg.ServerAddresPort)
 	agent.Run(cfg)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,22 +10,22 @@ import (
 	"metrics/internal/infra/store"
 )
 
-var flagRunAddr string
-
 // func init() {
 // flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 // }
 
 func main() {
+	var flagRunAddr string
+
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
 
-	if err := run(); err != nil {
+	if err := run(flagRunAddr); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
+func run(flagRunAddr string) error {
 	store, err := store.NewStore()
 	if err != nil {
 		return fmt.Errorf("failed to initialize a store: %w", err)
