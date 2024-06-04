99763531574b6fae91993ca834733903c52527c8
Пётр Павлов <pepavlov@cloud.ru>
2024-06-04 10:51:38 +0500

    Fix: the channel used with signal.Notify should be buffered

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -58,7 +58,7 @@ func run(cfg *config.Config) error {
 
 	// Wait for interrupt signal to gracefully shutdown the server with
 	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
 	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
